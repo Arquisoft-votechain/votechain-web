428080249af38806a18a6e508648f160620daa5b
Arvin <agarciagodos@itana.pe>
2023-05-22 20:28:33 -0500

    Se agrega login

--- src/app/acceso/acceso.module.ts
+++ src/app/acceso/acceso.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccesoRoutingModule } from './acceso-routing.module';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { IconsModule } from '../icons/icons.module';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    AccesoRoutingModule
+    AccesoRoutingModule,
+    IconsModule
   ]
 })
 export class AccesoModule { }
